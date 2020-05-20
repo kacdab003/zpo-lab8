806bf8de81f3ad426b3f950d2ce850f3f8828c78
Kacper <kdtw301@gmail.com>
2020-05-20 09:41:30 +0200

    Testing suite

--- src/app.js
+++ src/app.js
@@ -1,19 +1,24 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const dotenv = require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv").config();
 const app = express();
-const studentsRoutes = require('./routes/student');
-const port = process.env.PORT || 3000;
-
+const studentsRoutes = require("./routes/student");
+const port = process.env.NODE_ENV === "test" ? 3001 : 3000;
+const dbUrl =
+  process.env.NODE_ENV === "test"
+    ? process.env.MONGODB_URI_TEST
+    : process.env.MONGODB_URI;
 app.use(bodyParser.json());
-app.use('/students', studentsRoutes);
-mongoose.connect(process.env.MONGODB_URI, {
+app.use("/students", studentsRoutes);
+
+mongoose
+  .connect(dbUrl, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(result => {
-    app.listen(port, () => {
-        console.log('App runs on port ' + port);
-    });
-})
-//5e9172c29e58b24b53c23c68
+    useUnifiedTopology: true,
+  })
+  .then((result) => server);
+const server = app.listen(port, () => {
+  console.log("App runs on port " + port);
+});
+module.exports = { app, server };
